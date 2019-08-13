fc245be18458421d1fad81c62d2b3b3cf105995d
dceu00 <dceu00@gmail.com>
2019-08-13 09:41:36 -0400

    refactor: using Jsoup for Parsing

--- src/main/java/htmlextractor/App.java
+++ src/main/java/htmlextractor/App.java
@@ -2,6 +2,9 @@ package htmlextractor;
 
 import java.util.*;
 import java.io.File;
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+
 public class App 
 {
 
@@ -11,21 +14,33 @@ public class App
         System.out.println( "Hello World!" );
         //System.out.println("enter the url to extract data from:");
         System.out.println("In testing mode");
-        
-        try(Scanner scan = new Scanner(System.in)){
+
+        try (Scanner scan = new Scanner(System.in)){
             String s = "";
             //s = scan.nextLine();
+            s = testUrl;
+            System.out.println("Testing with Jsoup");
+            Document doc = HtmlDownloader.toDocument(s);
+            DocumentReader reader = new DocumentReader(doc);
 
-            s = testUrl; //toggle testmode
-
-            File txtFile = HtmlDownloader.toText(s);
-            // String txt = HtmlReader.readToString(txtFile);
-            Map<Integer, Map<String,List<String>>> recipeMap = HtmlReader.crawlAndMap(txtFile);
-            //if (recipeMap!= null) RecipeMapParser.makeRecipes(recipeMap);
-    
         }catch (Exception e){
-            System.out.println(e);
+            e.printStackTrace();
         }
+        
+        // try(Scanner scan = new Scanner(System.in)){ // old method
+        //     String s = "";
+        //     //s = scan.nextLine();
+
+        //     s = testUrl; //toggle testmode
+
+        //     File txtFile = HtmlDownloader.toText(s);
+        //     // String txt = HtmlReader.readToString(txtFile);
+        //     Map<Integer, Map<String,List<String>>> recipeMap = HtmlReader.crawlAndMap(txtFile);
+        //     //if (recipeMap!= null) RecipeMapParser.makeRecipes(recipeMap);
+    
+        // }catch (Exception e){
+        //     System.out.println(e);
+        // }
 
         
 
