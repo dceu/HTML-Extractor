a4736065888d3bf75faffb1fb78c4a878dd2952c
dceu00 <dceu00@gmail.com>
2019-08-13 09:51:08 -0400

    chore: commented out unused code

--- src/main/java/htmlextractor/HtmlDownloader.java
+++ src/main/java/htmlextractor/HtmlDownloader.java
@@ -16,27 +16,27 @@ public class HtmlDownloader{
     
 
 
-    static public File toText(String s) {
-        File temp = null;
+    // static public File toText(String s) {
+    //     File temp = null;
        
 
-        try {
-            temp = File.createTempFile("temp", ".txt");
-            System.out.println("created temp file at" + temp.getAbsolutePath()); // create Logger
-            temp.deleteOnExit();
-        } catch (IOException e) {
-            e.printStackTrace();
-		}
+    //     try {
+    //         temp = File.createTempFile("temp", ".txt");
+    //         System.out.println("created temp file at" + temp.getAbsolutePath()); // create Logger
+    //         temp.deleteOnExit();
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+	// 	}
         
-        try (InputStream inputStream = new URL(s).openStream()){
-            System.out.println("downloading from... " + s);
-            Files.copy(inputStream, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("copied html to file"); // create logger
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return temp;
-    }
+    //     try (InputStream inputStream = new URL(s).openStream()){
+    //         System.out.println("downloading from... " + s);
+    //         Files.copy(inputStream, temp.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    //         System.out.println("copied html to file"); // create logger
+    //     } catch (Exception e) {
+    //         System.out.println(e);
+    //     }
+    //     return temp;
+    // }
 
     static public Document toDocument(String s){
         
